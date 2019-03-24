6274f9558bf899f8362a67d855a041d6054885e8
Shub1427 <shub1493biswas@gmail.com>
2019-03-24 18:23:11 +0530

    feat: read file from root

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,12 @@
-use std::env::args;
+use std::env;
+use std::fs;
 
 fn main() {
-    let args: Vec<String> = args().collect();
-    println!("Arguments: {:#?}", args);
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[2];
+    println!("In file {}", filename);
+
+    let file_data = fs::read_to_string(filename).expect("Reading File Failed. Is File present??");
+
+    println!("File Data: {}", file_data);
 }
