ad7ce4e38e4f632cb204615a25edd3f6f97cf5cd
Shub1427 <shub1493biswas@gmail.com>
2019-03-24 20:41:55 +0530

    feat: add parse_config functions (incomplete)

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,10 +3,16 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[2];
-    println!("In file {}", filename);
+    let (_expression, filename) = parse_config(&args);
 
     let file_data = fs::read_to_string(filename).expect("Reading File Failed. Is File present??");
 
     println!("File Data: {}", file_data);
+}
+
+fn parse_config(args: &[String]) -> (&String, &String) {
+    let expression = &args[1]; // args[1] -> String, Therefore, &args[1] -> &String...
+    let filename = &args[2];
+
+    (expression, filename)
 }
