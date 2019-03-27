ca15bd05685467e50acaaa169878ea5d21540ece
Shub1427 <shub1493biswas@gmail.com>
2019-03-27 20:42:09 +0530

    feat: add error handling for file reads

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,12 +1,20 @@
-// Basically I separated out Config, only to test re-exports...
+// Error is a trait representing the basic expectations for error values, i.e., values of type E in Result<T, E>
+use std::error::Error;
 use std::fs;
 
+// Basically I separated out Config, only to test re-exports...
 mod config;
 pub use config::config::Config;
 
-pub fn run(config: Config) {
-	let file_data =
-        fs::read_to_string(config.get_filename()).expect("Reading File Failed. Is File present??");
+///
+/// This method is the main cycle, which runs the logical part of the application.
+/// It can throw error, but nothing is returned when it passes.
+///
+/// @returns Result<Ok, Err>
+///
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let file_data = fs::read_to_string(config.get_filename())?; // Propagating Error to caller.
+    println!("File Data: {}", file_data);
 
-    println!("File Data: {}", file_data);	
+    Ok(())
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,8 +6,12 @@ use minigrep;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
-    	println!("Problem parsing arguments: {}", err);
-    	process::exit(1);
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
     });
-    minigrep::run(config);
+
+    if let Err(err) = minigrep::run(config) {
+        println!("Problem reading File: {}", err);
+        process::exit(1);
+    }
 }
