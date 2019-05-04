64d5a6ac2c84894ea5cf8214a4bc1c7d11bb65a0
Shub1427 <shub1493biswas@gmail.com>
2019-05-04 19:32:00 +0530

    feat: incomplete iterator implementation

--- crack-coding/data-structures/src/bin/singly-linked-list.rs
+++ crack-coding/data-structures/src/bin/singly-linked-list.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 // https://leetcode.com/explore/learn/card/linked-list/
 // Also, will be using the guide https://rust-unofficial.github.io/too-many-lists/
 
@@ -11,16 +13,65 @@ struct Node<T> {
   next: Option<Box<Node<T>>>
 }
 
-fn main () {
-  let tail = Node {
-    data: 12,
-    next: None
-  };
+// Iterating in Rust can be done immutably/mutable, thus it is
+// a good convention to provide a separate immutable/mutable object to iter over
+// as when iterating, data can get consumed and ownership is lost, thus data gets invalidated
+// Also, It is important to provide a returning Iter object with same lifetime as to
+// LinkedList node, because compiler is not smart enough to figure it out.
+struct Iter<'a, T> {
+  // Will return a reference, instead of boxed reference, as we don't want the actual owner to
+  // lose ownership.
+  next: Option<&'a Node<T>>
+}
 
-  let head = Node {
-    data: 24,
-    next: Some(Box::new(tail)) // tail gets invalid, after changing the ownership...
-  };
+impl<T: Debug> Node<T> {
+  fn new(data: T) -> Node<T> {
+    Node {
+      data,
+      next: None
+    }
+  }
+
+  // Following is required for Making `Node` struct return an
+  // iterator on demand
+  fn iter(&self) -> Iter<T> {
+    Iter {
+      // `as_ref is required to convert `Option's` data into a reference,
+      // i.e., `Box will turn into `&Box`
+      // So, we need to dereference &Box twice nad then return &node of that.
+      // `node` here is a reference to `Box`, thus &**node is &Node
+      next: self.next.as_ref().map( |node| &**node )
+    }
+  }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+  type Item = &'a T;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    self.next.map(|node| {
+      // store the next Node reference, so that, we
+      self.next = node.next.as_ref().map( |node| &**node ); // same thing as inter()
+      &node.data // return a reference to data for loop, as we don't want to change ownership
+    })
+  }
+}
+
+fn add<T>(head: &mut Node<T>, node: Node<T>) {
+  head.next = Some(Box::new(node));
+}
+
+fn main () {
+  let mut head = Node::new(24);
+  add(&mut head, Node::new(12));
+  add(&mut head, Node::new(14));
+
+  println!("::Inside Loop::");
+  print!("[");
+  for data in head.iter() {
+    print!("{:?}, ", data);
+  }
+  println!("]");
 
   println!("List: {:?}", head);
 }
