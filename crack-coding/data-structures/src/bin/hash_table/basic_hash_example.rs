2dc1c79d7f5c8c46aa6e2cf7e5df3fd5a25fec94
Shub1427 <shub1493biswas@gmail.com>
2019-06-14 13:59:38 +0530

    fix: NonNull usage issues inside HashMap, though that's the correct way

--- crack-coding/data-structures/src/bin/hash_table/basic_hash_example.rs
+++ crack-coding/data-structures/src/bin/hash_table/basic_hash_example.rs
@@ -1,7 +1,12 @@
 // Well this is a very naive and simple implementation fo Hash map in rust.
 // It's just to learn Rust, via data structures...
 // Simplefied becoz I will concentrate only for number and strings as keys...
-use core::ptr::NonNull;
+
+// TODO: Read this and implement HashTable...
+// https://github.com/contain-rs/linked-hash-map/blob/master/src/lib.rs
+// https://doc.rust-lang.org/stable/book/ch19-01-unsafe-rust.html
+
+static MAX_CAPACITY: u8 = 30u8;
 
 pub trait HashBuilder {
   fn hash(&self) -> usize;
@@ -12,33 +17,56 @@ pub trait HashBuilder {
 // in Rust...
 impl HashBuilder for usize {
   fn hash(&self) -> usize {
-    *self % usize::max_value()
+    *self % MAX_CAPACITY as usize
   }
 }
 
 impl HashBuilder for String {
   fn hash(&self) -> usize {
-    self.chars().into_iter().map(|c| c as usize).sum()
+    // This algo, is not secure and not uniform...
+    // Different types of Hashing:
+    // - Additive Hashing
+    // - DJB2
+    // - Folding Hash
+    // - MD5
+    // - SHA-2 (Most secure of all, but less efficient)
+    self.chars().into_iter().map(|c| c as usize).sum::<usize>() % MAX_CAPACITY as usize
   }
 }
 
 // This is partially copied from -> https://github.com/rust-lang/hashbrown/blob/master/src/raw/mod.rs
 // To understand the advanced Rust use of generics and pointers for lazy loaded values...
-struct HashNode<T> {
-  data: NonNull<T>,
-  // TODO: add `next` property to iterate through List of Nodes...
+// struct HashNode<T> {
+//   data: NonNull<T>,
+//   // TODO: add `next` property to iterate through List of Nodes...
+// }
+
+// impl<T> HashNode<T> {
+//   fn new() -> Self {
+//     Self {
+//       data: NonNull::dangling(), // https://doc.rust-lang.org/std/ptr/struct.NonNull.html
+//     }
+//   }
+// }
+
+struct HashNode<K, V> {
+  key: K,
+  val: V,
+  next: Option<Box<HashNode<K, V>>>
 }
 
-impl<T> HashNode<T> {
-  fn new() -> Self {
+impl<K, V> HashNode<K, V> {
+  fn new(key: K, val: V) -> Self {
     Self {
-      data: NonNull::dangling(), // https://doc.rust-lang.org/std/ptr/struct.NonNull.html
+      key,
+      val,
+      next: None
     }
   }
 }
 
 pub struct HashMap<K: HashBuilder, V> {
-  table: HashNode<(K, V)>,
+  table: Vec<HashNode<K, V>>, // We need to store hashes in tables
 }
 
 impl<K, V> HashMap<K, V>
@@ -47,7 +75,13 @@ where
 {
   pub fn new() -> Self {
     Self {
-      table: HashNode::new(),
+      table: Vec::with_capacity(MAX_CAPACITY as usize)
     }
+  }
+
+  pub fn put(&mut self, key: K, val: V) {
+    let hash = key.hash();
+    // let node = &mut self.table[hash];
+    match node
   }
 }
