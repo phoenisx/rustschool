ba2cead853336ffba1d79f1f60d21e16e7f29b63
Shub1427 <shub1493biswas@gmail.com>
2019-06-20 21:19:33 +0530

    feat: sytactic sugar for Rust code

--- exercism/rust/reverse-string/src/reversed.rs
+++ exercism/rust/reverse-string/src/reversed.rs
@@ -1,9 +1,5 @@
-extern crate unicode_segmentation;
-
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(original: &str) -> String {
-  return UnicodeSegmentation::graphemes(original, true)
-    .rev()
-    .collect::<String>();
+  original.graphemes(true).rev().collect::<String>()
 }
