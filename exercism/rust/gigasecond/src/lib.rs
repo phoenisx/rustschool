1a50ce8e72277830531c8c13b1074d6395170cb5
Shub1427 <shub1493biswas@gmail.com>
2019-07-21 17:10:30 +0530

    feat: shorten Rust code sample

--- exercism/rust/gigasecond/src/lib.rs
+++ exercism/rust/gigasecond/src/lib.rs
@@ -1,8 +1,6 @@
 use chrono::{DateTime, Duration, Utc};
-use std::ops::Add;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-  let ten_to_nine = Duration::seconds(10i64.pow(9));
-  start.add(ten_to_nine)
+  start + Duration::seconds(10i64.pow(9))
 }
