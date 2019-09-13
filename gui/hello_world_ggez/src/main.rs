ae043aeba8000306d1e8eeb5e32be7444b36c459
Shub1427 <shub1493biswas@gmail.com>
2019-09-13 21:07:58 +0530

    feat: display text on window

--- gui/hello_world_ggez/src/main.rs
+++ gui/hello_world_ggez/src/main.rs
@@ -1,9 +1,20 @@
 // Learn from https://github.com/ggez/ggez/blob/master/docs/guides/HelloGgez.md
+// But the code is modified to work with actual graphics...
 
 use ggez::*;
 
 struct GameState {
     dt: std::time::Duration,
+    g_text: graphics::Text,
+}
+
+impl GameState {
+    fn new() -> Self {
+        GameState {
+            dt: std::time::Duration::new(0, 0),
+            g_text: graphics::Text::new("Hello World"),
+        }
+    }
 }
 
 impl event::EventHandler for GameState {
@@ -12,14 +23,21 @@ impl event::EventHandler for GameState {
         Ok(())
     }
 
-    fn draw(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        println!("Hello ggez! dt = {}ns", self.dt.subsec_nanos());
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::Color::from_rgba(50, 50, 50, 255));
+
+        // Not sure how efficient, setting the text on each render loop, is???
+        self.g_text = graphics::Text::new(format!("Hello World: {}ms", self.dt.subsec_millis()));
+        let dest_point = nalgebra::Point2::new(2.0, 10.0);
+        graphics::draw(ctx, &self.g_text, graphics::DrawParam::default().dest(dest_point))?;
+        graphics::present(ctx)?;
+        // println!("Hello ggez! dt = {}ns", self.dt.subsec_nanos());
         Ok(())
     }
 }
 
 pub fn main() {
-    let state = &mut GameState { dt: std::time::Duration::new(0, 0) };
+    let mut state = GameState::new();
 
     let c = conf::Conf::new();
     let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("hello_ggez", "subroto")
@@ -27,5 +45,5 @@ pub fn main() {
         .build()
         .unwrap();
 
-    event::run(ctx, event_loop, state).unwrap();
+    event::run(ctx, event_loop, &mut state).unwrap();
 }
