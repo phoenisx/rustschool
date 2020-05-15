e95060c08cb4f38ca96df8eea59dc3c72851868d
Shub1427 <shub1493biswas@gmail.com>
2020-05-15 23:26:48 +0530

    feat: proper windowing with pixel scaling for dense monitors

--- gui/gfx-rs/src/001-show_window/main.rs
+++ gui/gfx-rs/src/001-show_window/main.rs
@@ -1,37 +1,31 @@
-use std::mem::{ManuallyDrop};
-use std::{ptr};
+use std::mem::ManuallyDrop;
+use std::ptr;
+
+use gfx_hal::{window as hal_window, Backend, Instance};
 use winit::{
+    dpi::{LogicalSize, PhysicalSize, Size},
     event, event_loop, window,
-    dpi::{ Size, LogicalSize, PhysicalSize }
 };
-use gfx_hal::{
-    window as hal_window,
-    Backend,
-    Instance
-};
+
 #[cfg(feature = "dx12")]
 use gfx_backend_dx12 as back;
 #[cfg(feature = "metal")]
 use gfx_backend_metal as back;
 #[cfg(feature = "vulkan")]
 use gfx_backend_vulkan as back;
-use log::{debug};
 
-// #region Constants
-const DIMS: hal_window::Extent2D = hal_window::Extent2D {
-    width: 1024,
-    height: 768
-};
-// #endregion Constants
+use log::debug;
+use log4rs;
 
+const APP_NAME: &'static str = "Show Window";
+const WINDOW_SIZE: [u32; 2] = [1280, 768];
 
-// #region BackendState Structs/Impls
 struct BackendState<B: Backend> {
+    // Vulkan backend instance object
     instance: Option<B::Instance>,
+    // Vulkan backend surface object
     surface: ManuallyDrop<B::Surface>,
-    // adapter: AdapterState<B>,
-    /// Needs to be kept alive even if its not used directly
-    #[allow(dead_code)]
+    // `winit` Window object.
     window: window::Window,
 }
 
@@ -45,47 +39,64 @@ impl<B: Backend> Drop for BackendState<B> {
         }
     }
 }
-// #endregion BackendState Structs/Impls
 
 fn create_backend(
     wb: window::WindowBuilder,
-    event_loop: &event_loop::EventLoop<()>,
+    ev_loop: &event_loop::EventLoop<()>,
+    extent: hal_window::Extent2D,
 ) -> BackendState<back::Backend> {
-    let window = wb.build(event_loop).unwrap();
-    let instance =
-        back::Instance::create("Create Instance with Window", 1).expect("Failed to create an instance!");
+    let window = wb.build(ev_loop).unwrap();
+
+    let instance = back::Instance::create(APP_NAME, 1).expect("Failed to create an instance!");
     let surface = unsafe {
         instance
             .create_surface(&window)
             .expect("Failed to create a surface!")
     };
-    // let mut adapters = instance.enumerate_adapters();
+
     BackendState {
         instance: Some(instance),
-        // adapter: AdapterState::new(&mut adapters),
         surface: ManuallyDrop::new(surface),
         window,
     }
 }
 
-fn main() {
-    simple_logger::init().unwrap();
-    let event_loop = event_loop::EventLoop::new();
+fn build_window(
+    ev_loop: &event_loop::EventLoop<()>,
+) -> (window::WindowBuilder, hal_window::Extent2D) {
+    // We need to first get Logical and Physical Size of the screen
+    let (logical_window_size, physical_window_size) = {
+        let dpi = ev_loop.primary_monitor().scale_factor();
+        let logical: LogicalSize<u32> = WINDOW_SIZE.into();
+
+        // Phsical Size is the actual internal screen size, a factor of DPI
+        let physical: PhysicalSize<u32> = logical.to_physical(dpi);
+
+        (logical, physical)
+    };
 
     let window_builder = window::WindowBuilder::new()
-        .with_min_inner_size(Size::Logical(LogicalSize::new(
-            64.0, 64.0,
-        )))
-        .with_inner_size(Size::Physical(PhysicalSize::new(
-            DIMS.width,
-            DIMS.height,
-        )))
-        .with_title("colour-uniform".to_string());
+        .with_title(APP_NAME)
+        .with_inner_size(logical_window_size);
 
+    (
+        window_builder,
+        hal_window::Extent2D {
+            width: physical_window_size.width,
+            height: physical_window_size.height,
+        },
+    )
+}
+
+fn main() {
+    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+
+    let ev_loop = event_loop::EventLoop::new();
+    let (window_builder, extent) = build_window(&ev_loop);
     #[allow(unused_variables)]
-    let backend = create_backend(window_builder, &event_loop);
+    let backend = create_backend(window_builder, &ev_loop, extent);
 
-    event_loop.run(move |event, _, control_flow| {
+    ev_loop.run(move |event, _, control_flow| {
         *control_flow = event_loop::ControlFlow::Wait;
         match event {
             event::Event::WindowEvent { event, .. } =>
@@ -98,20 +109,19 @@ fn main() {
                     event::WindowEvent::Resized(dims) => {
                         debug!("RESIZE EVENT");
                     }
-                    event::WindowEvent::KeyboardInput {
-                        input:
-                            event::KeyboardInput {
-                                virtual_keycode,
-                                state: event::ElementState::Pressed,
-                                ..
-                            },
-                        ..
-                    } => {
-                        debug!("Keyboard input, {:?}", virtual_keycode);
+                    event::WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        // Will get called whenever the screen scale factor (DPI) changes,
+                        // like when user move the Window from one less DPI monitor
+                        // to other high scaled DPI Monitor.
+                        debug!("Scale Factor Change");
                     }
                     _ => (),
                 }
             }
+            event::Event::MainEventsCleared => {
+                debug!("MainEventsCleared");
+                backend.window.request_redraw();
+            },
             event::Event::RedrawRequested(_) => {
                 debug!("RedrawRequested");
             }
