b55c12bb5b65bdd019166f27e7fdc84745e4c954
Shub1427 <shub1493biswas@gmail.com>
2020-05-25 11:45:07 +0530

    feat: add image view code

--- gui/draw-cube/src/004-swap_chain/main.rs
+++ gui/draw-cube/src/004-swap_chain/main.rs
@@ -1,20 +1,15 @@
 use std::mem::ManuallyDrop;
 use std::ptr;
-use std::cell::RefCell;
-use std::rc::Rc;
 
 use gfx_hal::{
-    adapter::{Adapter, MemoryType},
-    command,
     prelude::*,
-    image,
-    format as hal_format,
-    queue::family::QueueGroup,
-    pool::CommandPoolCreateFlags,
-    window as hal_window, Backend, Features, Instance, Limits,
+    command,
+    pool::{CommandPoolCreateFlags},
+    format::{self as hal_format},
+    window as hal_window, Backend, Features, Instance,
 };
 use winit::{
-    dpi::{LogicalSize, PhysicalSize, Size},
+    dpi::{LogicalSize, PhysicalSize},
     event, event_loop, window,
 };
 
@@ -31,77 +26,60 @@ use log4rs;
 const APP_NAME: &'static str = "Show Window";
 const WINDOW_SIZE: [u32; 2] = [1280, 768];
 
-struct AdapterState<B: Backend> {
-    adapter: Option<Adapter<B>>,
-    memory_types: Vec<MemoryType>,
-    limits: Limits,
-}
-
-impl<B: Backend> AdapterState<B> {
-    fn new(adapters: &mut Vec<Adapter<B>>) -> Self {
-        // for (index, adapter) in adapters.iter().enumerate() {
-        //     debug!(
-        //         "Adapter[{}] Physical Device Limits: {:#?}",
-        //         index,
-        //         adapter.physical_device.memory_properties().memory_types
-        //     );
-        // }
-
-        // In my system, I have just one GPU Physical adapter, thus
-        // will only work with first instance.
-        let adapter = adapters.remove(0);
-
-        Self {
-            memory_types: adapter.physical_device.memory_properties().memory_types,
-            limits: adapter.physical_device.limits(),
-            adapter: Some(adapter),
-        }
-    }
-}
-
-struct DeviceState<B: Backend> {
-    physical_device: B::PhysicalDevice,
+struct Renderer<B: Backend> {
+    // Vulkan backend instance object
+    instance: B::Instance,
+    // Vulkan backend surface object
+    surface: ManuallyDrop<B::Surface>,
+    // Logical Device object
     device: B::Device,
-    queues: QueueGroup<B>,
+    // Swapchain instance
+    swapchain: Option<B::Swapchain>,
 }
 
-impl<B: Backend> DeviceState<B> {
-    fn new(adapter: Adapter<B>, surface: &B::Surface) -> Self {
-        let supported_family = adapter
-            .queue_families
-            .iter()
-            .find(|family| {
-                surface.supports_queue_family(family) && family.queue_type().supports_graphics()
-            })
-            .unwrap();
-
-        let mut gpu = unsafe {
-            adapter
-                .physical_device
-                .open(&[(supported_family, &[1.0])], Features::empty())
-                .unwrap()
+impl<B: Backend> Renderer<B> {
+    fn new(
+        instance: B::Instance,
+        mut surface: B::Surface,
+        init_extent: hal_window::Extent2D,
+    ) -> Self {
+        let mut adapters = instance.enumerate_adapters();
+        let (memory_types, limits, adapter) = {
+            let adapter = adapters.remove(0);
+            (
+                adapter.physical_device.memory_properties().memory_types,
+                adapter.physical_device.limits(),
+                adapter,
+            )
         };
 
-        Self {
-            physical_device: adapter.physical_device,
-            device: gpu.device,
-            queues: gpu.queue_groups.pop().unwrap(),
-        }
-    }
-}
+        let (device, queues, supported_family) = {
+            let supported_family = adapter
+                .queue_families
+                .iter()
+                .find(|family| {
+                    surface.supports_queue_family(family) && family.queue_type().supports_graphics()
+                })
+                .unwrap();
 
-struct FrameBufferState<B: Backend> {
-    command_pool: Option<B::CommandPool>,
-    command_buffer: B::CommandBuffer,
-}
+            let mut gpu = unsafe {
+                adapter
+                    .physical_device
+                    .open(&[(supported_family, &[1.0])], Features::empty())
+                    .unwrap()
+            };
 
-impl<B: Backend> FrameBufferState<B> {
-    fn new(deviceState: &DeviceState<B>) -> Self {
+            (
+                gpu.device,
+                gpu.queue_groups.pop().unwrap(),
+                supported_family,
+            )
+        };
+
         let (command_pool, mut command_buffer) = unsafe {
-            let mut command_pool = deviceState
-                .device
+            let mut command_pool = device
                 .create_command_pool(
-                    deviceState.queues.family,
+                    queues.family,
                     CommandPoolCreateFlags::empty()
                 )
                 .expect("Out of memory");
@@ -113,102 +91,59 @@ impl<B: Backend> FrameBufferState<B> {
             (command_pool, command_buffer)
         };
 
-        Self {
-            command_pool: Some(command_pool),
-            command_buffer,
-        }
-    }
-}
-
-struct SwapchainState<B: Backend> {
-    swapchain: Option<B::Swapchain>,
-    backbuffer: Option<Vec<B::Image>>,
-    deviceState: Rc<RefCell<DeviceState<B>>>,
-    extent: image::Extent,
-    format: hal_format::Format,
-}
-
-impl<B: Backend> SwapchainState<B> {
-    fn new(backendState: &mut BackendState<B>, deviceState: Rc<RefCell<DeviceState<B>>>) -> Self {
         // Get Surface Capabilities
-        let caps = backendState
-            .surface
-            .capabilities(&deviceState.borrow().physical_device);
+        let (swapchain, backbuffer, extent, format) = {
+            let caps = surface
+                .capabilities(&adapter.physical_device);
 
-        let supported_formats = backendState
-            .surface
-            .supported_formats(&deviceState.borrow().physical_device);
-        // We need a supported format for the OS Window, so that Images drawn on
-        // Swapchain are of that same format.
-        let format = supported_formats.map_or(hal_format::Format::Rgba8Srgb, |formats| {
-            formats
-                .iter()
-                .find(|format| format.base_format().1 == hal_format::ChannelType::Srgb)
-                .map(|format| *format)
-                .unwrap_or(formats[0])
-        });
+            let supported_formats = surface
+                .supported_formats(&adapter.physical_device);
+            // We need a supported format for the OS Window, so that Images drawn on
+            // Swapchain are of that same format.
+            let format = supported_formats.map_or(hal_format::Format::Rgba8Srgb, |formats| {
+                formats
+                    .iter()
+                    .find(|format| format.base_format().1 == hal_format::ChannelType::Srgb)
+                    .map(|format| *format)
+                    .unwrap_or(formats[0])
+            });
 
-        debug!("Selected Format:: {:#?}", format);
+            debug!("Selected Format:: {:#?}", format);
 
-        let swap_config = hal_window::SwapchainConfig::from_caps(&caps, format, backendState.extent);
-        let extent = swap_config.extent.to_extent();
-        let (swapchain, backbuffer) = unsafe {
-            deviceState
-                .borrow()
-                .device
-                .create_swapchain(&mut backendState.surface, swap_config, None)
-                .expect("Can't create swapchain")
+            let swap_config = hal_window::SwapchainConfig::from_caps(&caps, format, init_extent);
+            let extent = swap_config.extent.to_extent();
+            let (swapchain, backbuffer) = unsafe {
+                device
+                    .create_swapchain(&mut surface, swap_config, None)
+                    .expect("Can't create swapchain")
+            };
+
+            (
+                swapchain,
+                backbuffer,
+                extent,
+                format
+            )
         };
 
-        SwapchainState {
+        Renderer {
+            instance,
+            surface: ManuallyDrop::new(surface),
+            device,
             swapchain: Some(swapchain),
-            backbuffer: Some(backbuffer),
-            deviceState,
-            extent,
-            format,
         }
     }
 }
 
-/// Ok I understand now, why this Drop is required.
-/// We need to destroy swapchain from device first
-/// then only actualy drop the actual instance of self.swapchain
-impl<B: Backend> Drop for SwapchainState<B> {
+impl<B: Backend> Drop for Renderer<B> {
     fn drop(&mut self) {
         unsafe {
-            self.deviceState
-                .borrow()
-                .device
-                .destroy_swapchain(self.swapchain.take().unwrap());
-        }
-    }
-}
-
-struct BackendState<B: Backend> {
-    // Vulkan backend instance object
-    instance: Option<B::Instance>,
-    // Vulkan backend surface object
-    surface: ManuallyDrop<B::Surface>,
-    // Vulkan backend surface object
-    adapterState: AdapterState<B>,
-    extent: hal_window::Extent2D,
-    // `winit` Window object.
-    window: window::Window,
-}
-
-impl<B: Backend> BackendState<B> {
-    fn update_extent(&mut self, extent: hal_window::Extent2D) {
-        self.extent = extent;
-    }
-}
-
-impl<B: Backend> Drop for BackendState<B> {
-    fn drop(&mut self) {
-        if let Some(instance) = &self.instance {
-            unsafe {
-                let surface = ManuallyDrop::into_inner(ptr::read(&self.surface));
-                instance.destroy_surface(surface);
-            }
+            // up here ManuallyDrop gives us the inner resource with ownership
+            // where `ptr::read` doesn't do anything just reads the resource
+            // without manipulating the actual memory
+            let surface = ManuallyDrop::into_inner(ptr::read(&self.surface));
+            self.instance.destroy_surface(surface);
+            self.device.destroy_swapchain(self.swapchain.take().unwrap());
         }
     }
 }
@@ -216,8 +151,7 @@ impl<B: Backend> Drop for BackendState<B> {
 fn create_backend(
     wb: window::WindowBuilder,
     ev_loop: &event_loop::EventLoop<()>,
-    extent: hal_window::Extent2D,
-) -> BackendState<back::Backend> {
+) -> (back::Instance, back::Surface, window::Window) {
     let window = wb.build(ev_loop).unwrap();
 
     let instance = back::Instance::create(APP_NAME, 1).expect("Failed to create an instance!");
@@ -227,15 +161,11 @@ fn create_backend(
             .expect("Failed to create a surface!")
     };
 
-    let mut adapters = instance.enumerate_adapters();
-
-    BackendState {
-        instance: Some(instance),
-        surface: ManuallyDrop::new(surface),
-        adapterState: AdapterState::new(&mut adapters),
-        extent,
-        window,
-    }
+    (
+        instance,
+        surface,
+        window
+    )
 }
 
 fn build_window(
@@ -270,27 +200,9 @@ fn main() {
 
     let ev_loop = event_loop::EventLoop::new();
     let (window_builder, extent) = build_window(&ev_loop);
-    #[allow(unused_variables)]
-    let mut backend = create_backend(window_builder, &ev_loop, extent);
+    let (instance, surface, window) = create_backend(window_builder, &ev_loop);
 
-    let deviceState = DeviceState::new(
-        backend.adapterState.adapter.take().unwrap(),
-        &backend.surface
-    );
-
-    let frameBufferState = FrameBufferState::new(
-        &deviceState
-    );
-
-    // This is temporary and will be removed in later sections
-    let rcDeviceState = Rc::new(
-        RefCell::new(deviceState)
-    );
-
-    let swapchainState = SwapchainState::new(
-        &mut backend,
-        Rc::clone(&rcDeviceState)
-    );
+    let renderer = Renderer::<back::Backend>::new(instance, surface, extent);
 
     ev_loop.run(move |event, _, control_flow| {
         *control_flow = event_loop::ControlFlow::Wait;
@@ -302,34 +214,26 @@ fn main() {
                         *control_flow = event_loop::ControlFlow::Exit
                     }
                     event::WindowEvent::Resized(dims) => {
-                        backend.update_extent(hal_window::Extent2D {
-                            width: dims.width,
-                            height: dims.height,
-                        });
                         debug!("RESIZE EVENT");
                     }
                     event::WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
                         // Will get called whenever the screen scale factor (DPI) changes,
                         // like when user move the Window from one less DPI monitor
                         // to other high scaled DPI Monitor.
-                        backend.update_extent(hal_window::Extent2D {
-                            width: new_inner_size.width,
-                            height: new_inner_size.height,
-                        });
                         debug!("Scale Factor Change");
                     }
                     _ => (),
                 }
             }
             event::Event::MainEventsCleared => {
-                // debug!("MainEventsCleared");
-                backend.window.request_redraw();
+                debug!("MainEventsCleared");
+                // window.request_redraw();
             }
             event::Event::RedrawRequested(_) => {
-                // debug!("RedrawRequested");
+                debug!("RedrawRequested");
             }
             event::Event::RedrawEventsCleared => {
-                // debug!("RedrawEventsCleared");
+                debug!("RedrawEventsCleared");
             }
             _ => (),
         }
